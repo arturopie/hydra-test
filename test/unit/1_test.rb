e61ad30182089ce1afebc31b8ed4abb7f60fe471
Arturo Pie <arturop@nulogy.com>
2012-02-10 13:15:19 -0500

    removes a bunch of sleeps

--- test/unit/1_test.rb
+++ test/unit/1_test.rb
@@ -1,9 +1,9 @@
-require 'test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 class DummyTest1 < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "dummy test" do
-    sleep 10
+    sleep 1
     assert true
   end
 end
--- test/unit/3_test.rb
+++ test/unit/3_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DummyTest3 < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "dummy test" do
-    sleep 10
+#    sleep 10
     assert true
   end
 end
